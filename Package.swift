659865beee3412d5406b3b17bb57dfdfad786301
George Scott <gscott@gekkoto.com>
2023-06-09 18:32:14 -0700

    Initial workflow

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let purchasesVersion = "4.21.1" // Updated by workflow action
+let purchaseshecksum = "215b7b37b3085c9522da8ededce74cb81162b542d35e4aefea634cbda61c2376" // Updated by workflow action
+
+
+let package = Package(
+    name: "purchases-ios",
+    platforms: [
+        .macOS(.v10_13),
+        .watchOS("6.2"),
+        .tvOS(.v11),
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "RevenueCat", targets: ["RevenueCat"]),
+    ],
+    dependencies: [ ],
+    targets: [
+        .binaryTarget(name: "RevenueCat",
+                      url: "https://github.com/swift-xcframeworks/purchases-ios/releases/download/\(purchasesVersion)/RevenueCat.xcframework.zip",
+                      checksum: purchasesVersion)
+    ]
+)
